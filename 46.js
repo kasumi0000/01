6fb09f796fa839563c3d3400b864a24c60f1c928
kasumi0000 <113447186+kasumi0000@users.noreply.github.com>
2022-11-08 22:41:32 +0900

    Add files via upload

---
+++ 46.js
@@ -0,0 +1,6 @@
+var hex = 0xfa;
+console.log(hex);
+var hex = 0x5a;
+console.log(hex);
+var hex = 0xb4;
+console.log(hex);
---
+++ 47.js
@@ -0,0 +1,6 @@
+var hex = 0xa9;
+console.log(hex);
+var hex = 0xb3;
+console.log(hex);
+var hex = 0xb3;
+console.log(hex);
